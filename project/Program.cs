07f34eb66b32a6e6fcba7b4f8379a4d8c8b9f689
Thiago Fernando <thiagof1503@gmail.com>
2016-09-26 09:22:57 -0300

    Primeiro commit

---
+++ project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro");
+
+            int x= int.Parse(Console.ReadLine());
+            int y = 2*x;
+            Console.WriteLine("DOBRO = " + y);
+            Console.ReadLine();
+        }
+    }
+}
